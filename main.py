18ab1dd035e0dc51bd73ec69af89dd7b1dc29bc2
Jens Streck <jens.streck@gmail.com>
2021-03-07 17:52:39 -0600

    capture endpoint

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from io import BytesIO
-from time import sleep
-from picamera import PiCamera
-import requests
+from fastapi import FastAPI
 
-my_stream = BytesIO()
-camera = PiCamera()
-camera.start_preview()
-# Camera warm-up time
-sleep(2)
-camera.capture(my_stream, 'jpeg')
+app = FastAPI()
 
-r = requests.post('http://192.168.50.156:8080/images/name.jpg', data=my_stream.getbuffer() ,headers={'Content-Type': 'image/jpeg', 'Content-Length': str(my_stream.getbuffer().nbytes)})
+@app.get('/capture')
+def captureImage():
+  from io import BytesIO
+  from time import sleep
+  from picamera import PiCamera
+  import requests
+
+  my_stream = BytesIO()
+  camera = PiCamera()
+  camera.start_preview()
+  # Camera warm-up time
+  sleep(1)
+  camera.capture(my_stream, 'jpeg')
+
+  r = requests.post('http://192.168.50.156:8080/images/name.jpg', data=my_stream.getbuffer() ,headers={'Content-Type': 'image/jpeg', 'Content-Length': str(my_stream.getbuffer().nbytes)})
+  return r.json()
