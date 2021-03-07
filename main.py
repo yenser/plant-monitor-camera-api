f9143ea43a2f3b747ffb273cd5d2249264ebc7d1
Jens Streck <jens.streck@gmail.com>
2021-03-07 22:24:05 +0000

    upload images to server

--- main.py
+++ main.py
@@ -1,66 +1,13 @@
-from fastapi import FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
-from camera import Camera
-from database import Database
-from images import toByteArray
-from starlette.responses import StreamingResponse
-import cv2
-import io
-import datetime
+from io import BytesIO
+from time import sleep
+from picamera import PiCamera
+import requests
 
-db = Database()
-db.connect()
+my_stream = BytesIO()
+camera = PiCamera()
+camera.start_preview()
+# Camera warm-up time
+sleep(2)
+camera.capture(my_stream, 'jpeg')
 
-camera = Camera()
-
-app = FastAPI()
-
-origins = ["*"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-@app.get('/capture/{name}')
-def capturePicture(name):
-
-  x = datetime.datetime.now()
-  name += x.strftime("-%y-%m-%d-%H-%M-%S")
-
-  image = camera.capture()
-  is_success, image_buff = toByteArray(image)
-
-  if(is_success):
-    id = db.saveImage(name, image_buff)
-    return {"id": id }
-
-  raise HTTPException(status_code=500, detail="Failed to capture an image")
-
-@app.get('/images/{id}')
-def getImage(id):
-  image = db.getImageById(id)
-
-  if(image is None):
-    raise HTTPException(status_code=404)
-
-  return StreamingResponse(io.BytesIO(image), media_type="image/jpg")
-
-
-@app.get('/version')
-def version():
-  return {"version": 'v0.1.0'}
-
-@app.get('/version/db')
-def dbVersion():
-  db_version = db.getVersion()
-
-  return db_version
-
-@app.get('/dbsize')
-def dbSize():
-  size = db.getDatabaseSize()
-  return {"size": size}
+r = requests.post('http://192.168.50.156:8080/images/name.jpg', data=my_stream.getbuffer() ,headers={'Content-Type': 'image/jpeg', 'Content-Length': str(my_stream.getbuffer().nbytes)})
