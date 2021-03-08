e9a0349e9499fec0a62a976fbd2e32593f496b27
Jens Streck <jens.streck@gmail.com>
2021-03-07 18:47:46 -0600

    fix up capture function

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 from fastapi import FastAPI
+from io import BytesIO
+from time import sleep
+import picamera
+import requests
 
 app = FastAPI()
 
 @app.get('/capture')
 def captureImage():
-  from io import BytesIO
-  from time import sleep
-  from picamera import PiCamera
-  import requests
+  with picamera.PiCamera() as camera:
+    image_stream = BytesIO()
+    camera.start_preview()
+    # Camera warm-up time
+    sleep(1)
+    camera.capture(image_stream, 'jpeg')
 
-  my_stream = BytesIO()
-  camera = PiCamera()
-  camera.start_preview()
-  # Camera warm-up time
-  sleep(1)
-  camera.capture(my_stream, 'jpeg')
-
-  r = requests.post('http://192.168.50.156:8080/images/name.jpg', data=my_stream.getbuffer() ,headers={'Content-Type': 'image/jpeg', 'Content-Length': str(my_stream.getbuffer().nbytes)})
-  return r.json()
+    r = requests.post('http://192.168.50.156:8080/images/name.jpg',
+      data=image_stream.getbuffer(),
+      headers={'Content-Type': 'image/jpeg', 'Content-Length': str(image_stream.getbuffer().nbytes)})
+    return r.json()
