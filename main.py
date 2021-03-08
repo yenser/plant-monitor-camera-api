14be9f61d429bf35a689b5161fcf385841b679f7
Jens Streck <jens.streck@gmail.com>
2021-03-07 20:30:40 -0600

    cors

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from io import BytesIO
 from time import sleep
 import picamera
 import requests
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/capture')
 def captureImage():
